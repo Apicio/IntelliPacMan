c49925a2dbf6617b84e69c5c729e3265ab383dd8
apicio <philyo1@gmail.com>
2016-06-21 11:25:55 +0200

    Bugfix: ADDIO SOGNO PROFONDITA' ENORME. :( PROBLEMA: con profondità 1 funziona anche meglio ?!?

--- src/DecisionTree/Greedy.java
+++ src/DecisionTree/Greedy.java
@@ -2,6 +2,10 @@ package DecisionTree;
 
 import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.LinkedList;
+
+import org.apache.commons.lang3.ArrayUtils;
+
 import pacman.EvaluationHeuristic;
 import pacman.controllers.Controller;
 import pacman.game.Constants.GHOST;
@@ -15,11 +19,17 @@ import pacman.game.Game;
  */
 public class Greedy extends Controller<MOVE>
 {
-	static private int DEPTH = 100000;
+	static private int DEPTH = 1;
 	EnumMap<GHOST, MOVE> ghostMoves = new EnumMap<GHOST, MOVE>(GHOST.class);
-	
+	static private LinkedList<Integer> values;
+	Tree tree;
+	public Greedy(){
+		tree = new Tree(DEPTH);
+		values = new LinkedList<Integer>();
+	}
 	
 	public int dfsSearch(Node node, int life){
+
 		Game gameState = node.getPredecessor().getGameState().copy();
 		if (!isValidMove(gameState.getPossibleMoves(gameState.getPacmanCurrentNodeIndex()), node.getMove()))
 			return Integer.MIN_VALUE;
@@ -28,11 +38,12 @@ public class Greedy extends Controller<MOVE>
 		node.setGameState(gameState);
 			
 		ArrayList<Node> neighbors = node.getNeighbors();
-		if (neighbors == null || node.getGameState().gameOver()|| node.getGameState().getPacmanNumberOfLivesRemaining() < life) 
+
+		if (neighbors.size() == 0 || node.getGameState().gameOver()|| node.getGameState().getPacmanNumberOfLivesRemaining() < life) 
 			return EvaluationHeuristic.evaluateGameState(node.getGameState()); // end of branch return heuristic
 
 		int bestValue = Integer.MIN_VALUE;
-		for (Node neighbor : neighbors) {		
+		for (Node neighbor : neighbors) {
 			int value = dfsSearch(neighbor,life);
 			if (value > bestValue)
 				bestValue = value;
@@ -54,7 +65,6 @@ public class Greedy extends Controller<MOVE>
 		this.ghostMoves.put(GHOST.INKY, game.getGhostLastMoveMade(GHOST.INKY));
 		this.ghostMoves.put(GHOST.PINKY, game.getGhostLastMoveMade(GHOST.PINKY));
 		this.ghostMoves.put(GHOST.SUE, game.getGhostLastMoveMade(GHOST.SUE));
-		Tree tree = new Tree(DEPTH);
 		tree.getHeadNode().setGameState(game);
 		ArrayList<Node> neighbors = tree.getHeadNode().getNeighbors();
 		
@@ -62,7 +72,17 @@ public class Greedy extends Controller<MOVE>
 		int rightValue = dfsSearch(neighbors.get(1),game.getPacmanNumberOfLivesRemaining());
 		int upValue = dfsSearch(neighbors.get(2),game.getPacmanNumberOfLivesRemaining());
 		int downValue = dfsSearch(neighbors.get(3),game.getPacmanNumberOfLivesRemaining());
-		
+
 		return EvaluationHeuristic.getBestMove(leftValue, rightValue, upValue, downValue);
+	}
+	
+	private int mean(LinkedList<Integer> l){
+		int acc=0;
+		for(Integer i : l){
+			acc+=i;
+		}
+		System.out.println("masn");
+		return acc/l.size();
+		
 	}
 }
